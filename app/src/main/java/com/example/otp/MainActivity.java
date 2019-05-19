e19ac685635f96d181b97f8430b5ba93b27aa6ba
cool1259 <cool1259@nate.com>
2019-05-19 23:49:14 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/otp/MainActivity.java
+++ app/src/main/java/com/example/otp/MainActivity.java
@@ -1,50 +1,119 @@
 package com.example.otp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper Vf;
+    Button BtnSignIn, BtnSignUp;
+    EditText inputID, inputPW;
+    HttpPost httppost;
+    StringBuffer buffer;
+    HttpResponse response;
+    HttpClient httpclient;
+    List<NameValuePair> nameValuePairs;
+    ProgressDialog dialog = null;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText idText = (EditText) findViewById(R.id.emailInput);
-        final EditText passwordText = (EditText) findViewById(R.id.passwordInput);
-        Button button3 = (Button) findViewById(R.id.button3);
-        Button button4=(Button) findViewById(R.id.button4);
-        button3.setOnClickListener(new View.OnClickListener() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        BtnSignUp = (Button)findViewById(R.id.button4);
+        BtnSignIn = (Button)findViewById(R.id.button3);
+        inputID = (EditText)findViewById(R.id.emailInput);
+        inputPW = (EditText)findViewById(R.id.passwordInput);
+        tv = (TextView)findViewById(R.id.textView2);
+
+        BtnSignIn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), startmenu.class);
-                String id = idText.getText().toString();
-                String pass = passwordText.getText().toString();
-
-                intent.putExtra("id", id);
-                intent.putExtra("pass", pass);
-
-
-                MainActivity.this.startActivity(intent);
+                dialog = ProgressDialog.show(MainActivity.this, "",
+                        "Validating user...", true);
+                new Thread(new Runnable() {
+                    public void run() {
+                        login();
+                    }
+                }).start();
             }
         });
-           button4.setOnClickListener(new View.OnClickListener(){
-
-
-                public void onClick (View v){
-                Intent registerintent = new Intent(getApplicationContext(), registerActtivity.class);
-                    startActivity(registerintent);
-            }
-
-             });
     }
 
+    void login() {
+        try {
+            httpclient = new DefaultHttpClient();
+            httppost = new HttpPost("http://ubuntu@13.125.102.51/login2.php");
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("ID", inputID.getText().toString()));
+            nameValuePairs.add(new BasicNameValuePair("password", inputPW.getText().toString()));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            response = httpclient.execute(httppost);
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            final String response = httpclient.execute(httppost, responseHandler);
+            System.out.println("Response : " + response);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tv.setText("Response from PHP : " + response);
+                    dialog.dismiss();
+                }
+            });
 
+            if (response.contains("login success")) {  //php에서 온 response가 login success면 로그인됨
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                    }
+                });
+               // Intent intent = new Intent(getApplicationContext(), startmenu.class);
+            //    startActivity(intent);
+                startActivity((new Intent(MainActivity.this, startmenu.class)));
+                finish();
+            } else {                                          // 그게 아니면 실패
+                Toast.makeText(MainActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch(Exception e)
+        {
+            dialog.dismiss();
+            System.out.println("Exception : " + e.getMessage());
+        }
+    }
 
+    public void CliSignUp(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), registerActtivity.class);
+        startActivity(intent);
+       // Intent intent = new Intent(this, registerActtivity.class);
+        //startActivity(intent);
+    }
 }
